d514a297c3d3c3aa9fc6b202b75e41613ae9ee8f
H4mit <h4mid.taheri@gmail.com>
2019-01-22 15:41:35 +0330

    feat - add bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import * as Loadable from "react-loadable";
 import './core/http/interceptors';
 import {AppConfig} from './core/config/AppConfig';
 import Loading  from './utils/loadings/Loading';
+import 'bootstrap/dist/css/bootstrap.css';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const LoadableComponent = Loadable({
     loader: () => import('./App'),
--- src/layout/master.layout.tsx
+++ src/layout/master.layout.tsx
@@ -4,25 +4,51 @@ import MasterRoutes from 'src/routes/master';
 import { IRoute } from 'src/routes';
 
 const switchRoutes = (
-    <Switch>
-      {MasterRoutes.map((route: IRoute, key) => {
-        if (route.redirect)
-          return <Redirect from={route.path} to={route.to} key={key} />;
-        return <Route path={route.path} exact={true} component={route.component} key={key} />;
-      })}
-    </Switch>
-  );
-  
+  <Switch>
+    {MasterRoutes.map((route: IRoute, key) => {
+      if (route.redirect)
+        return <Redirect from={route.path} to={route.to} key={key} />;
+      return <Route path={route.path} exact={true} component={route.component} key={key} />;
+    })}
+  </Switch>
+);
+
 
 export default class MasterLayout extends React.Component<any, any> {
 
-    public render() {
-        return <div>
-            <h1>Master Layout!!</h1>
-            {switchRoutes}
-            <hr/>
-            <Link to="/">Home</Link> <Link to="/about">About</Link>
+  public render() {
+    return <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">Start Bootstrap</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home
+                <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <h1>Master Layout!!</h1>
+      {switchRoutes}
+      <hr />
+      <Link to="/">Home</Link> <Link to="/about">About</Link>
 
-            </div>;
-    }
+    </div>;
+  }
 }
